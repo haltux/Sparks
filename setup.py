7c715902eff0304a2338645252c21cd9471d42ce
unknown <Julien@.(none)>
2010-12-04 14:48:10 +0100

    version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'Sparks',
-    version = '0.4.4',
+    version = '0.4.5',
     description = 'A vectorial shooter',
     author = 'Haltux',
     url = 'https://github.com/haltux',
